ae8cfef2390a71303ccbf0336ec1e916619cb380
chencu5958 <82279230+chencu5958@users.noreply.github.com>
2026-03-14 23:50:54 +0800

    feat(nitro-server): 更新预渲染路由配置
    移除 '/404.html' 路由不再预渲染，
    添加 prerender 标志检查以优化 SSR 上下文创建

--- packages/nitro-server/src/runtime/utils/renderer/app.ts
+++ packages/nitro-server/src/runtime/utils/renderer/app.ts
@@ -8,10 +8,11 @@ import unheadOptions from '#internal/unhead-options.mjs'
 // @ts-expect-error virtual file
 import { NUXT_NO_SSR, NUXT_SHARED_DATA } from '#internal/nuxt/nitro-config.mjs'
 
-const PRERENDER_NO_SSR_ROUTES = new Set(['/index.html', '/200.html', '/404.html'])
+const PRERENDER_NO_SSR_ROUTES = new Set(['/index.html', '/200.html'])
 
 export function createSSRContext (event: H3Event): NuxtSSRContext {
   const url = event.url.pathname + event.url.search + event.url.hash
+    const isPrerenderNoSSRRoute = import.meta.prerender && PRERENDER_NO_SSR_ROUTES.has(url)
   const ssrContext: NuxtSSRContext = {
     url,
     event,
